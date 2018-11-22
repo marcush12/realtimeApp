36e3902a2ba54ab8e6b7678a26d65448cea88b35
marcush12 <marcos012santos@gmail.com>
2018-11-22 18:57:18 -0200

    Vuetify, Vue Router and Initial Components

---
+++ resources/assets/js/components/AppFooter.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-footer class="pa-3">
+    Annettes
+    <v-spacer></v-spacer>
+    <div>&copy; {{ new Date().getFullYear() }}</div>
+  </v-footer>
+</template>
+
+<script>
+export default {
+
+
+}
+</script>
+
+<style lang="css" scoped>
+</style>
